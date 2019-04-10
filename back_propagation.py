6fa74743a493315acce70579731cd015a304ea47
jucho2725 <cju2725@gmail.com>
2019-04-10 14:38:14 +0900

    minor change

--- back_propagation.py
+++ back_propagation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 14 17:37:46 2018
+Created on Mon Jan 14 17:37:46 2019
 
 @author: Jin Uk, Cho
 original source: https://github.com/jgabriellima/backpropagation
